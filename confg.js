e7cb735f87a7afdb820633469867579ae06223a9
RMD125 <ramdaneagbagni20@gmail.com>
2025-09-28 02:10:12 +0000

    Create confg.js

---
+++ confg.js
@@ -0,0 +1,15 @@
+{
+  "ownerNumber": "22896190934@s.whatsapp.net",
+  "ownerName": "RMD125",
+  "botName": "RMD-BOT",
+  "prefix": ".",
+  "sessionName": "session",
+  "maxUploadSize": 100,
+  "admins": ["22896190934@s.whatsapp.net"],
+  "groups": ["120363043023449399@g.us"],
+  "security": {
+    "antiDelete": true,
+    "autoReact": true,
+    "autoViewStatus": true
+  }
+}
